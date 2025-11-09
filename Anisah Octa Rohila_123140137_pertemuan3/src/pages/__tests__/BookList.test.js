c83be508d7c69b6f35f320e58f7f8646594385af
Anisah Octa Rohila <anshoctr@gmail.com>
2025-11-09 13:17:36 +0700

    update p4

--- Anisah Octa Rohila_123140137_pertemuan3/src/pages/__tests__/BookList.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import BookList from '../components/BookList';
-
-test('renders empty state when no books', () => {
-  render(<BookList books={[]} onEdit={() => {}} onDelete={() => {}} />);
-  expect(screen.getByText(/Belum Ada Buku/i)).toBeInTheDocument();
-});
